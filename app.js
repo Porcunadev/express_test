cebc7776627ace031fc5dde1c0ff0dd32539cdd8
PorcunaDev <porcunadev@pop-os.localdomain>
2020-11-24 10:20:01 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+require('dotenv').config();
+app.set('view engine', 'ejs');
+app.use('/', require('./routes/hello.js'));
+/* app.get('/', (req,res) =>{
+    res.send('hello');
+}) */
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log(`Listening on Port: ${PORT}`);
+})
